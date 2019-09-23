961f8204fdb7d43f8252b40deeb299097abe1643
Matt Plichta <bigcatplichta@gmail.com>
2019-09-23 18:08:37 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,5 +1,14 @@
 class Artist 
   attr_accessor :name
   
+  def initialize(name)
+    :name = name 
+    @songs = [] 
+  end 
   
+  def add_song
+    
+  end 
+  
+  def add_song_by_name(name, )
 end 
