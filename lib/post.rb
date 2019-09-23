b962ad51b4c67097c5957c81aed1b2aad39f44cc
Matt Plichta <bigcatplichta@gmail.com>
2019-09-23 19:13:28 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,8 +1,13 @@
 class Post 
   attr_accessor :title, :author 
+  @@all = []
   
   def initialize(title)
     @title = title 
     self.author.posts << self
+  end 
+  
+  self.all 
+    @@all
   end 
 end 
